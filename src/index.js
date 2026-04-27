19cfef65467ab17558791fe8d45083551d4e1f2b
firstflipofficial-lab <firstflipofficial@gmail.com>
2026-04-27 19:34:55 -0400

    Update src/index.js with main application code on 2026-04-27 23:34:38 UTC

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FirstFlipUI from './App';
+
+ReactDOM.render(<React.StrictMode><FirstFlipUI /></React.StrictMode>, document.getElementById('root'));
